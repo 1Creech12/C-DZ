a70d5e26769df4d3135d12c3bc18d69302cc564a
1Creech12 <167149547+1Creech12@users.noreply.github.com>
2025-09-17 23:36:40 +0900

    Add files via upload

---
+++ dz.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Задание 1:\n\n         Дельтаплан\nМеж нами памяти туман\nТы как во сне, ты как во сне\nНаверное, только дельтаплан\nПоможет мне, поможет мне\nНаивно это и смешно\nНо так легко моим плечам\nУже зовет меня в полет\nМой дельтаплан, мой дельтаплан\n\n               Валерий Леонтьев\n\n\nЗадание 2:\n\n";
+    cout << "Каждый\t\t\t\t\t\t\tEvery\n\tОхотник\t\t\t\t\t\t\tHunter\n\t\tЖелает\t\t\t\t\t\t\tWants\n\t\t\tЗнать\t\t\tИли\t\t\t\tTo know\n\t\t\t\tГде\t\t\t\t\t\t\tWhere\n\t\t\t\t\tСидит\t\t\t\t\t\t\tDoes Pheasant\n\t\t\t\t\t\tФазан\t\t\t\t\t\t\tSit\n\n\nЗадание 3:\n\n";
+    cout << "Продаются: Телефоны\nМодель: Nokia Lumia 1520\tNokia N8\t Nokia 808 PureView\nЗвонить по: +7-924-711-96-01\n   +7 924\t|   +7 924\t|   +7 924\t|   +7 924\t|   +7 924\n      711\t|      711\t|      711\t|      711\t|      711\n     96 01\t|     96 01\t|     96 01\t|     96 01\t|     96 01\n";
+}
