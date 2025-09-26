e77c8ba2e9cd47357af21411e2c0c6ed88cb6753
1Creech12 <167149547+1Creech12@users.noreply.github.com>
2025-09-26 17:02:19 +0900

    Add files via upload

---
+++ 26.09.2025.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    /*
+    Задание 1. 
+    Пользователь с клавиатуры вводит 5 оценок
+    студента. Определить, допущен ли студент к экзамену.
+    Студент получает допуск, если его средний балл 4 балла
+    и выше.
+
+    int n1,n2,n3,n4,n5;
+    char ball;
+    cout << "Введите свои оценки:\n";
+    cin >> n1 >> n2 >> n3 >> n4 >> n5;
+    ball = (n1+n2+n3+n4+n5)/5;
+    cout << ball;
+    switch (ball)
+    {
+    case 4:
+    case 5:
+        cout << "Вы допущены";
+        break;
+    default:
+        cout << "Вы не допущены";
+        break;
+    }
+    return 0;
+    */
+
+    /*
+    Задание 2. 
+    Пользователь вводит с клавиатуры число. Если
+    оно четное, умножить его на три, иначе — поделить на
+    два. Результат вывести на экран.
+    */
+   int n1;
+   double number;
+   cout << "Введите число: ";
+   cin >> n1;
+    switch (n1 % 2) {
+        case 0:
+            number = n1*3.0;
+            cout << "Число четное\n";
+            cout << number << "\n";
+            break;
+        case 1:
+        case -1:
+            number = n1/2.0;
+            cout << "Число не четное\n";
+            cout << number << "\n";
+            break;
+    }
+}
