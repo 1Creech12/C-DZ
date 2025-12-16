e6505893a6c4aad2d6d0015bbddc440df77be887
1Creech12 <aleksandrnac109@gmail.com>
2025-12-16 16:46:39 +0900

    modified:   numerical methods/20.12.25.cpp 	modified:   numerical methods/output/20.12.25 	modified:   numerical methods/output/20.12.25.dSYM/Contents/Resources/DWARF/20.12.25

--- numerical methods/20.12.25.cpp
+++ numerical methods/20.12.25.cpp
@@ -1,44 +1,85 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
-double func(double x) {
-    return sin(x) * exp(-x);
+double f(double x) {
+    return sin(x);
 }
 
-double integral(double a, double b, double eps) {
-    double oldv = 0, newv = 1e9;
-    int n = 5;
-    
-    while (fabs(newv - oldv) > eps) {
-        n *= 2;
-        oldv = newv;
-        double step = (b - a) / n;
-        newv = 0;
-        
-        for (int i = 0; i < n; i++) {
-            double x = a + i * step;
-            newv += func(x);
-        }
-        newv *= step;
+double leftrect(double a, double b, int n) {
+    double h = (b - a) / n;
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += f(a + i * h);
     }
-    return newv;
+    return sum * h;
+}
+
+double rightrect(double a, double b, int n) {
+    double h = (b - a) / n;
+    double sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += f(a + i * h);
+    }
+    return sum * h;
+}
+
+double midrect(double a, double b, int n) {
+    double h = (b - a) / n;
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += f(a + (i + 0.5) * h);
+    }
+    return sum * h;
+}
+
+double trapez(double a, double b, int n) {
+    double h = (b - a) / n;
+    double sum = (f(a) + f(b)) / 2;
+    for (int i = 1; i < n; i++) {
+        sum += f(a + i * h);
+    }
+    return sum * h;
+}
+
+double simpson(double a, double b, int n) {
+    if (n % 2 != 0) n++;
+    double h = (b - a) / n;
+    double sum = f(a) + f(b);
+    
+    for (int i = 1; i < n; i++) {
+        if (i % 2 == 0) {
+            sum += 2 * f(a + i * h);
+        } else {
+            sum += 4 * f(a + i * h);
+        }
+    }
+    return sum * h / 3;
 }
 
 int main() {
-    cout << "\tВычисление интеграла\n";
-    cout << "=======================================\n";
+    cout << "\tФормулы Ньютона-Котеля\n";
+    cout << "=================================\n";
     
     double a, b, eps;
-    cout << "Начало (a): ";
+    int n;
+    
+    cout << "Введите a: ";
     cin >> a;
-    cout << "Конец (b): ";
+    cout << "Введите b: ";
     cin >> b;
-    cout << "Точность: ";
+    cout << "Введите n (число отрезков): ";
+    cin >> n;
+    cout << "Введите точность: ";
     cin >> eps;
     
-    double result = integral(a, b, eps);
-    cout << "Результат: " << result << endl;
+    cout << "\n\tРезультаты:\n";
+    cout << "1. Левых прямоугольников: " << leftrect(a, b, n) << endl;
+    cout << "2. Правых прямоугольников: " << rightrect(a, b, n) << endl;
+    cout << "3. Средних прямоугольников: " << midrect(a, b, n) << endl;
+    cout << "4. Трапеций: " << trapez(a, b, n) << endl;
+    cout << "5. Симпсона (парабол): " << simpson(a, b, n) << endl;
     
     return 0;
 }
