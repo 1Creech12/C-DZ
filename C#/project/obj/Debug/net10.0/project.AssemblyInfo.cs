6e9343358016a5f4ff2b322913eab313f9691df6
1Creech12 <aleksandrnac109@gmail.com>
2026-06-04 23:47:21 +0900

    modified:   C#/project/obj/Debug/net10.0/project.AssemblyInfo.cs 	modified:   C#/project/obj/Debug/net10.0/project.AssemblyInfoInputs.cache 	modified:   WPF/2/obj/Debug/net10.0/2.AssemblyInfo.cs 	modified:   WPF/2/obj/Debug/net10.0/2.AssemblyInfoInputs.cache 	modified:   WPF/Rider/1/1/obj/Debug/net10.0/1.AssemblyInfo.cs 	modified:   WPF/Rider/1/1/obj/Debug/net10.0/1.AssemblyInfoInputs.cache

--- C#/project/obj/Debug/net10.0/project.AssemblyInfo.cs
+++ C#/project/obj/Debug/net10.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af813b2bbab065cfbaa1d1e8ab6a4a7ff213e5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187eb681ad4cb952e1f683c826282f0510b724b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
